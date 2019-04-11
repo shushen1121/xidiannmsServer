99cc20cbf27deef1e3b38b58c25555284f4d8f68
shushen1121 <1121807045@qq.com>
2019-04-11 18:43:37 +0800

    新增api

---
+++ controller/ctls/ctl_getLinkDetail.js
@@ -0,0 +1,40 @@
+const logInfo=require('../logInfo');
+module.exports=function(req,res){
+  var resData;
+  // 未登录
+  if(!req.session.authority){
+    resData=logInfo.log_321;
+    res.json(resData);
+    return;
+  }
+  // 权限不足
+  else if(parseInt(req.session.authority)<1){
+    resData=logInfo.log_322;
+    res.json(resData);
+    return;
+  }
+  // 必要参数不为空
+  if(!req.body.link_id){
+    resData=logInfo.log_302;
+    res.json(resData);
+    return;
+  }else{
+    var cmd=`select * from link where link_id=${req.body.link_id}`;
+  }
+
+  // 执行数据库命令
+  global.dbQuery(cmd,errCallback,resCallback);
+
+  // 错误回调
+  function errCallback(){
+    resData=logInfo.log_301;
+    res.json(resData);
+  }
+
+  // 成功回调
+  function resCallback(dbRes){
+    resData=logInfo.log_220;
+    resData.data=dbRes;
+    res.json(resData);
+  }
+}
--- controller/route/getter.js
+++ controller/route/getter.js
@@ -1,21 +1,23 @@
 module.exports=function(app){
   // 获取拓扑结构
-  const ctl_topology = require('../ctls/ctl_getTopology');
+  const ctl_getTopology = require('../ctls/ctl_getTopology');
   app.post('/getTopology',function(req, res){
-    ctl_topology(req, res);
+    ctl_getTopology(req, res);
   })
 
   // 获取设备详情
+  const ctl_getMachineDetail = require('../ctls/ctl_getMachineDetail');
   app.post('/getMachineDetail',function(req, res){
-    res.send('敬请期待');
+    ctl_getMachineDetail(req, res);
   })
 
   // 获取链路详情
+  const ctl_getLinkDetail = require('../ctls/ctl_getLinkDetail');
   app.post('/getLinkDetail',function(req, res){
-    res.send('敬请期待');
+    ctl_getLinkDetail(req, res);
   })
 
-  // 获取告警
+  // 获取告警信息
   const ctl_getWarning = require('../ctls/ctl_getWarning');
   app.post('/getWarning',function(req, res){
     ctl_getWarning(req, res);
