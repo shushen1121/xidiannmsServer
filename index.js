96e8ef015ce4f74f0c4e86cdb9da01abb7d1a881
shushen1121 <1121807045@qq.com>
2019-04-11 18:48:25 +0800

    开启websocket功能

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ global.EventEmitter=new events.EventEmitter();
 
 const database=require('./database/main');
 const wsToken=require('./database/wsToken');
+
 const controller=require('./controller/main');
+const webSocket=require('./websocket/main');
