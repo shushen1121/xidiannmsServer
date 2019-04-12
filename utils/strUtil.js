1b3245d20afc6e9bd2ef37baee989e3a8ec0184b
shushen1121 <1121807045@qq.com>
2019-04-12 22:18:23 +0800

    strUtil

---
+++ utils/strUtil.js
@@ -0,0 +1,19 @@
+module.exports={
+  // {key:val,key:val} -> key='val',key='val'
+  jsObjToSQLProp_update:function(data){
+    data=JSON.stringify(data);
+    data=data.replace(/{?"([^":,{}]+)":"?([^":,{}]+)"?}?/g,"$1='$2'");
+    return data;
+  },
+  // {key:val,key:val} -> (key,key) values (val,val)
+  jsObjToSQLProp_insert:function(data){
+    data=JSON.stringify(data);
+    var arrKey=[],arrVal=[];
+    data.replace(/{?"([^":,{}]+)":"?([^":,{}]+)"?}?/g,function(data){
+      arrKey.push(data.replace(/{?"([^":,{}]+)":"?([^":,{}]+)"?}?/,"$1"));
+      arrVal.push(data.replace(/{?"([^":,{}]+)":"?([^":,{}]+)"?}?/,"$2"));
+    })
+    data=`(${arrKey.join(',')}) values (${arrVal.join(',')})`;
+    return data;
+  }
+}
