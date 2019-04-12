0df44dd10f3584a8a9ccc0db7284774bb2f250b6
shushen1121 <1121807045@qq.com>
2019-04-12 22:17:13 +0800

    完成set接口

--- controller/route/setter.js
+++ controller/route/setter.js
@@ -1,14 +1,17 @@
 module.exports=function(app){
   // 修改设备详情
+  const ctl_setMachineDetail = require('../ctls/ctl_setMachineDetail');
   app.post('/setMachineDetail',function(req, res){
-    res.send('敬请期待');
+    ctl_setMachineDetail(req, res);
   })
   // 修改链路详情
+  const ctl_setLinkDetail = require('../ctls/ctl_setLinkDetail');
   app.post('/setLinkDetail',function(req, res){
-    res.send('敬请期待');
+    ctl_setLinkDetail(req, res);
   })
   // 修改告警信息
+  const ctl_setWarning = require('../ctls/ctl_setWarning');
   app.post('/setWarning',function(req, res){
-    res.send('敬请期待');
+    ctl_setWarning(req, res);
   })
 }
