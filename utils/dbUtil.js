e7459115ba8d8d6514274d4311d5846029b1118d
shushen1121 <1121807045@qq.com>
2019-04-14 23:21:11 +0800

    增加判断链路修改是否影响拓扑方法

--- utils/dbUtil.js
+++ utils/dbUtil.js
@@ -5,5 +5,12 @@ module.exports={
     }else if(data[0].machine_id){
       return 'machine';
     }
+  },
+  whetherTopologyChange:function(data){
+    if(data.from_machine||data.to_machine){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
