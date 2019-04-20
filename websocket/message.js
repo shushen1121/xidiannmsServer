7ea3b2d65179b0b7a7b4643a5c22d5ca7ea603a7
shushen1121 <1121807045@qq.com>
2019-04-20 17:12:06 +0800

    改code

--- websocket/message.js
+++ websocket/message.js
@@ -10,7 +10,7 @@ module.exports=function(ws){
     try{
       var data = JSON.parse(wsReq);
     }catch{
-      var wsRes={ code:301, message:'token', data:'参数错误' };
+      var wsRes={ code:300, message:'token', data:'参数错误' };
       ws.send(JSON.stringify(wsRes));
       return;
     }
@@ -18,19 +18,19 @@ module.exports=function(ws){
     if(data.token){
       global.wsToken(data.token,errCallback,resCallback);
     }else{
-      var wsRes={ code:302, message:'token', data:'无效参数' };
+      var wsRes={ code:300, message:'token', data:'无效参数' };
       ws.send(JSON.stringify(wsRes));
     }
     // errCallback
     function errCallback(){
-      var wsRes={ code:303, message:'token', data:'数据库错误' };
+      var wsRes={ code:300, message:'token', data:'数据库错误' };
       ws.send(JSON.stringify(wsRes));
     }
     // resCallback
     function resCallback(res){
       var wsRes;
       if(!res[0]){
-        wsRes={ code:304, message:'token', data:'无权限连接' };
+        wsRes={ code:300, message:'token', data:'无权限连接' };
       }else{
         wsRes={ code:200, message:'token', data:'获取权限成功' };
         ws.token=data.token;
